c3909463df160e951576bdaacfe89ce74fc900b5
maskiilovmai <maikimlong2405@gmail.com>
2021-10-27 17:28:11 +0700

    main

--- commands/help.js
+++ commands/help.js
@@ -123,7 +123,7 @@ module.exports = {
   Discord Music Bot Version: v${require("../package.json").version}
   [✨ Support Server](${
     client.botconfig.SupportServer
-  }) | [GitHub](https://github.com/SudhanPlayz/Discord-MusicBot) | [Dashboard](${client.botconfig.Website}) | By [SudhanPlayz](https://github.com/SudhanPlayz)`);
+  }) | [GitHub](https://github.com/maskiilovmai) | [Dashboard](${client.botconfig.Website}) | By [Maskiilov Mai](https://www.facebook.com/maskiilovmai)`);
       if (!args) return interaction.send(Embed);
       else {
         let cmd =
